184eb631559c952b67b84fed109fe7bd87117bd4
Albin Rajesh <albinrajesh2596@gmail.com>
2025-04-21 18:23:13 +0530

    pco update by albin

--- frontend/src/services/axiosConfig.js
+++ frontend/src/services/axiosConfig.js
@@ -1,35 +1,35 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://192.168.0.114:8787", 
-  timeout: 10000,
-  headers: {
-      "Content-Type": "application/json", 
-    },
-  });
-  
-  export default api;
-
-
 // import axios from "axios";
 
-// const getBackendURL = () => {
-//   const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
-//   const port = window.location.port || 8787;  
-  
-//   const baseURL = isLocalhost 
-//     ? `http://localhost:${port}`  
-//     : `http://${window.location.hostname}:${port}`; 
-
-//   return baseURL;
-// };
-
 // const api = axios.create({
-//   baseURL: getBackendURL(),
+//   baseURL: "http://192.168.0.114:8787", 
 //   timeout: 10000,
 //   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
+//       "Content-Type": "application/json", 
+//     },
+//   });
+  
+//   export default api;
 
-// export default api;
+
+import axios from "axios";
+
+const getBackendURL = () => {
+  const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+  const port = window.location.port || 8787;  
+  
+  const baseURL = isLocalhost 
+    ? `http://localhost:${port}`  
+    : `http://${window.location.hostname}:${port}`; 
+
+  return baseURL;
+};
+
+const api = axios.create({
+  baseURL: getBackendURL(),
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
